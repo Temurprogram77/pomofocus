c0b02e9a7999c2ae29c3bdb5aaf90d6977b1ffb3
Temur Coder <temurnarzullayev810@gmail.com>
2025-02-17 20:42:47 +0300

    added

--- script.js
+++ script.js
@@ -3,21 +3,20 @@ let time = document.querySelector(".time");
 
 let btn_start = document.querySelector(".start");
 let btn_pause = document.querySelector(".pause");
-let timerDisplay = document.querySelector(".timer");
-let min = 20;
+let min = 1;
 let sec = 0;
-let interval;
+let intervall;
 
 btn_start.addEventListener("click", () => {
   btn_pause.classList.remove("hidden");
   btn_start.classList.add("hidden");
   btn_pause.classList.add("style");
 
-  if (interval) clearInterval(interval);
+  if (intervall) clearInterval(intervall);
 
-  interval = setInterval(() => {
+  intervall = setInterval(() => {
     if (min === 0 && sec === 0) {
-      clearInterval(interval);
+      clearInterval(intervall);
       return;
     }
 
@@ -35,7 +34,12 @@ btn_start.addEventListener("click", () => {
 function updateDisplay() {
   let formattedMin = min < 10 ? "0" + min : min;
   let formattedSec = sec < 10 ? "0" + sec : sec;
-  timerDisplay.innerText = `${formattedMin}:${formattedSec}`;
+  time.innerText = `${formattedMin}:${formattedSec}`;
+  if (formattedSec == 0 && formattedMin == 0) {
+    setTimeout(()=>{
+        time.innerText = "20:00";
+    },1000)
+  }
 }
 
 updateDisplay();
@@ -50,42 +54,3 @@ updateDisplay();
 
 //   }, 1000);
 // });
-btn_pause.addEventListener("click", () => {
-  btn_start.classList.remove("hidden");
-  btn_pause.classList.add("hidden");
-  btn_start.classList.add("style");
-});
-
-let minutes = 20;
-let seconds = 0;
-let interval;
-
-function startTimer() {
-  if (interval) clearInterval(interval);
-  interval = setInterval(() => {
-    if (minutes === 0 && seconds === 0) {
-      clearInterval(interval);
-      alert("Vaqt tugadi!");
-      return;
-    }
-
-    if (seconds === 0) {
-      minutes--;
-      seconds = 59;
-    } else {
-      seconds--;
-    }
-
-    updateDisplay();
-  }, 1000);
-}
-
-function updateDisplay() {
-  let min = minutes < 10 ? "0" + minutes : minutes;
-  let sec = seconds < 10 ? "0" + seconds : seconds;
-  document.getElementById("timer").innerText = `${min}:${sec}`;
-}
-
-document.getElementById("startBtn").addEventListener("click", startTimer);
-
-updateDisplay();
